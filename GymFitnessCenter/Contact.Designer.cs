83e1d566f77d377a26314479d6617c14e6b550cf
Amir Abbas <shaikh4851@gmail.com>
2024-01-29 11:52:35 +0500

    Uploading Full Project

---
+++ GymFitnessCenter/Contact.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace GymFitnessCenter
+{
+    partial class Contact
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Contact));
+            label1 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label4 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            button3 = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point);
+            label1.ForeColor = Color.Crimson;
+            label1.Location = new Point(198, 7);
+            label1.Name = "label1";
+            label1.Size = new Size(236, 29);
+            label1.TabIndex = 42;
+            label1.Text = "FITNESS CENTER";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Cursor = Cursors.Hand;
+            label3.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.ForeColor = Color.Crimson;
+            label3.Location = new Point(677, 7);
+            label3.Name = "label3";
+            label3.Size = new Size(31, 29);
+            label3.TabIndex = 44;
+            label3.Text = "X";
+            label3.Click += label3_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Constantia", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.Crimson;
+            label2.Location = new Point(209, 49);
+            label2.Name = "label2";
+            label2.Size = new Size(163, 29);
+            label2.TabIndex = 45;
+            label2.Text = "CONTACT US";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Calibri", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.ForeColor = Color.Maroon;
+            label5.Location = new Point(281, 112);
+            label5.Name = "label5";
+            label5.Size = new Size(131, 29);
+            label5.TabIndex = 47;
+            label5.Text = "Amir Abbas";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Calibri", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.ForeColor = Color.Maroon;
+            label6.Location = new Point(178, 112);
+            label6.Name = "label6";
+            label6.Size = new Size(89, 29);
+            label6.TabIndex = 48;
+            label6.Text = "NAME :";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Calibri", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.ForeColor = Color.Maroon;
+            label4.Location = new Point(134, 161);
+            label4.Name = "label4";
+            label4.Size = new Size(129, 29);
+            label4.TabIndex = 50;
+            label4.Text = "Phone No. :";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Calibri", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.ForeColor = Color.Maroon;
+            label7.Location = new Point(281, 161);
+            label7.Name = "label7";
+            label7.Size = new Size(174, 29);
+            label7.TabIndex = 49;
+            label7.Text = "+92 3414851115";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Calibri", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label8.ForeColor = Color.Maroon;
+            label8.Location = new Point(188, 205);
+            label8.Name = "label8";
+            label8.Size = new Size(81, 29);
+            label8.TabIndex = 52;
+            label8.Text = "Email :";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Calibri", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label9.ForeColor = Color.Maroon;
+            label9.Location = new Point(281, 205);
+            label9.Name = "label9";
+            label9.Size = new Size(293, 29);
+            label9.TabIndex = 51;
+            label9.Text = "amirabbas8674@gmail.com";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Calibri", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label10.ForeColor = Color.Maroon;
+            label10.Location = new Point(160, 256);
+            label10.Name = "label10";
+            label10.Size = new Size(107, 29);
+            label10.TabIndex = 54;
+            label10.Text = "Address :";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Calibri", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label11.ForeColor = Color.Maroon;
+            label11.Location = new Point(281, 256);
+            label11.Name = "label11";
+            label11.Size = new Size(176, 29);
+            label11.TabIndex = 53;
+            label11.Text = "27/TDA Bhakkar";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Calibri", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label12.ForeColor = Color.Maroon;
+            label12.Location = new Point(281, 284);
+            label12.Name = "label12";
+            label12.Size = new Size(73, 29);
+            label12.TabIndex = 55;
+            label12.Text = "30000";
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Crimson;
+            button3.Cursor = Cursors.Hand;
+            button3.FlatAppearance.BorderColor = Color.Crimson;
+            button3.FlatAppearance.BorderSize = 2;
+            button3.FlatAppearance.MouseDownBackColor = Color.Crimson;
+            button3.FlatAppearance.MouseOverBackColor = Color.FromArgb(152, 3, 33);
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Cambria", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.ForeColor = SystemColors.ButtonHighlight;
+            button3.Location = new Point(231, 347);
+            button3.Margin = new Padding(3, 2, 3, 2);
+            button3.Name = "button3";
+            button3.Size = new Size(128, 38);
+            button3.TabIndex = 59;
+            button3.Text = "Back";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(62, 9);
+            pictureBox1.Margin = new Padding(3, 2, 3, 2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(117, 88);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 60;
+            pictureBox1.TabStop = false;
+            // 
+            // Contact
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(728, 396);
+            Controls.Add(pictureBox1);
+            Controls.Add(button3);
+            Controls.Add(label12);
+            Controls.Add(label10);
+            Controls.Add(label11);
+            Controls.Add(label8);
+            Controls.Add(label9);
+            Controls.Add(label4);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label2);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Contact";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Contact";
+            Load += Contact_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label3;
+        private Label label2;
+        private Label label5;
+        private Label label6;
+        private Label label4;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Button button3;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ GymFitnessCenter/View.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace GymFitnessCenter
+{
+    partial class View
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(View));
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            MDGV = new DataGridView();
+            searchtb = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)MDGV).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Cursor = Cursors.Hand;
+            label3.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.ForeColor = Color.Crimson;
+            label3.Location = new Point(1012, -2);
+            label3.Name = "label3";
+            label3.Size = new Size(31, 29);
+            label3.TabIndex = 25;
+            label3.Text = "X";
+            label3.Click += label3_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Constantia", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = Color.Crimson;
+            label2.Location = new Point(422, 75);
+            label2.Name = "label2";
+            label2.Size = new Size(194, 29);
+            label2.TabIndex = 24;
+            label2.Text = "VIEW MEMBERS";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point);
+            label1.ForeColor = Color.Crimson;
+            label1.Location = new Point(422, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(236, 29);
+            label1.TabIndex = 23;
+            label1.Text = "FITNESS CENTER";
+            // 
+            // MDGV
+            // 
+            MDGV.AllowUserToAddRows = false;
+            MDGV.AllowUserToDeleteRows = false;
+            MDGV.AllowUserToResizeColumns = false;
+            MDGV.AllowUserToResizeRows = false;
+            MDGV.BackgroundColor = Color.White;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = SystemColors.Control;
+            dataGridViewCellStyle1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            MDGV.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            MDGV.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = SystemColors.Window;
+            dataGridViewCellStyle2.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = Color.Red;
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.False;
+            MDGV.DefaultCellStyle = dataGridViewCellStyle2;
+            MDGV.GridColor = Color.Blue;
+            MDGV.Location = new Point(131, 178);
+            MDGV.Margin = new Padding(3, 2, 3, 2);
+            MDGV.Name = "MDGV";
+            MDGV.ReadOnly = true;
+            MDGV.RowHeadersWidth = 51;
+            MDGV.RowTemplate.DividerHeight = 2;
+            MDGV.RowTemplate.Height = 29;
+            MDGV.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            MDGV.Size = new Size(814, 290);
+            MDGV.TabIndex = 39;
+            MDGV.CellContentClick += MDGV_CellContentClick;
+            // 
+            // searchtb
+            // 
+            searchtb.BackColor = Color.AliceBlue;
+            searchtb.Font = new Font("Constantia", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            searchtb.ForeColor = Color.Crimson;
+            searchtb.Location = new Point(413, 146);
+            searchtb.Margin = new Padding(3, 2, 3, 2);
+            searchtb.Name = "searchtb";
+            searchtb.PlaceholderText = "Enter Name";
+            searchtb.Size = new Size(146, 24);
+            searchtb.TabIndex = 40;
+            searchtb.TextChanged += searchtb_TextChanged;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Crimson;
+            button1.Cursor = Cursors.Hand;
+            button1.FlatAppearance.BorderColor = Color.Crimson;
+            button1.FlatAppearance.BorderSize = 2;
+            button1.FlatAppearance.MouseDownBackColor = Color.Crimson;
+            button1.FlatAppearance.MouseOverBackColor = Color.FromArgb(152, 3, 33);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Cambria", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(577, 137);
+            button1.Margin = new Padding(3, 2, 3, 2);
+            button1.Name = "button1";
+            button1.Size = new Size(80, 33);
+            button1.TabIndex = 41;
+            button1.Text = "Search";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Crimson;
+            button2.Cursor = Cursors.Hand;
+            button2.FlatAppearance.BorderColor = Color.Crimson;
+            button2.FlatAppearance.BorderSize = 2;
+            button2.FlatAppearance.MouseDownBackColor = Color.Crimson;
+            button2.FlatAppearance.MouseOverBackColor = Color.FromArgb(152, 3, 33);
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Cambria", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.ForeColor = SystemColors.ButtonHighlight;
+            button2.Location = new Point(686, 137);
+            button2.Margin = new Padding(3, 2, 3, 2);
+            button2.Name = "button2";
+            button2.Size = new Size(82, 33);
+            button2.TabIndex = 42;
+            button2.Text = "Refresh";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Crimson;
+            button3.Cursor = Cursors.Hand;
+            button3.FlatAppearance.BorderColor = Color.Crimson;
+            button3.FlatAppearance.BorderSize = 2;
+            button3.FlatAppearance.MouseDownBackColor = Color.Crimson;
+            button3.FlatAppearance.MouseOverBackColor = Color.FromArgb(152, 3, 33);
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Cambria", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.ForeColor = SystemColors.ButtonHighlight;
+            button3.Location = new Point(472, 472);
+            button3.Margin = new Padding(3, 2, 3, 2);
+            button3.Name = "button3";
+            button3.Size = new Size(128, 35);
+            button3.TabIndex = 43;
+            button3.Text = "Back";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(257, 30);
+            pictureBox1.Margin = new Padding(3, 2, 3, 2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(150, 88);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 44;
+            pictureBox1.TabStop = false;
+            // 
+            // View
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(1046, 518);
+            Controls.Add(pictureBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(searchtb);
+            Controls.Add(MDGV);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "View";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "View";
+            Load += View_Load;
+            ((System.ComponentModel.ISupportInitialize)MDGV).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private DataGridView MDGV;
+        private TextBox searchtb;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private PictureBox pictureBox1;
+    }
+}
